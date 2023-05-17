5f4ab96a2ac3d3072950db1e8b8384452989bfaa
h3nrey <pedro.novaes.education@gmail.com>
2023-05-17 20:34:08 -0300

    frontend: conecting with Github Api and clean up the code

---
+++ frontend/src/Components/Copyright.tsx
@@ -0,0 +1,15 @@
+export default function Copyright() {
+  return (
+    <div className="text-small leading-relaxed text-gray-200">
+      Feito com 💜 no NLW da{' '}
+      <a
+        className="underline hover:text-gray-100"
+        href="https://github.com/h3nrey"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Pedro Novaes
+      </a>
+    </div>
+  )
+}
---
+++ frontend/src/Components/EmptyMemorie.tsx
@@ -0,0 +1,12 @@
+export default function EmptyMemorie() {
+  return (
+    <div className="flex flex-1 items-center justify-center">
+      <p className="w-[360px] text-center leading-relaxed">
+        Você ainda não registrou nenhuma lembrança, comece a criar agora!{' '}
+        <a href="" className="underline hover:text-gray-50">
+          criar agora
+        </a>
+      </p>
+    </div>
+  )
+}
